c5e87fbd569805b0bfdd431904d79f1c8d2c7776
NoSwear4324 <spiridonovaroslav10@gmail.com>
2026-02-08 11:12:52 +0200

    noo

--- generics_app_2/MyList.cs
+++ generics_app_2/MyList.cs
@@ -1,4 +1,6 @@
-using System.Security.Cryptography.X509Certificates;
+using System;
+using System.Collections.Generic;
+using System.Text;
 
 namespace generics_app_2
 {
@@ -6,37 +8,58 @@ namespace generics_app_2
     {
         public T? Data { get; set; }
         public Node<T>? Next { get; set; }
-
         public Node(T? data)
         {
             Data = data;
             Next = null;
         }
     }
-
-public class MyList<T>
-{
-    private Node<T>? Head;
-    private Node<T>? Tail;
-
-    public int Count { get; set; }
-    
-    public MyList()
+    public class MyList<T>
     {
-        Head = null;
-        Tail = null;
-        Count = 0;
-    }
+        public Node<T>? Head;
+        public Node<T>? Tail;
+        public int Count { get; set; }
+        public MyList()
+        {
+            Head = null; 
+            Tail = null; 
+            Count = 0;
+        }
+        public void AddLast(T data) //Add last O(1)
+        {
+            Node<T> newNode = new Node<T>(data);
+            if(Head == null)
+            {
+                Head = Tail = newNode;
+                return;
+            }
+            Tail?.Next = newNode;
+            Tail = newNode;
+        }
+        public void AddFirst(T data) //Add first O(1)
+        {
 
-    public void AddLast(T data) //Add last 0(1)
-    {
-        
-    }
-
-    public void AddFirst(T data) //Add first 0(1)
-    {
-        
+        }
+        //Додати методи:
+        //ToString, Find, Sort, this["First"], this["Last"]
+        //Reverse, Clear, Contains, Remove(T item),
+        //RemoveLast, RemoveFirst
+        //InsertAfter(T Value) -> Вставити новий елемент, після першого входження T Value
+        //InsertBefore(T Value) -> Вставити новий елемент, перед першого входження T Value
+        //SwapPairs() -> Поміняти місцями кожну пару сусідніх нод приклад:
+        //Було: 1 -> 2 -> 3 -> 4 -> 5 -> null
+        //Стало: 2 -> 1 -> 4 -> 3 -> 5 -> null
+        //
+        public override string ToString()
+        {
+            string result = "";
+            Node<T>? current = Head;
+            while(current != null)
+            {
+                result += current.Data + "\t";
+                current = current.Next;
+            }
+            return result;
+        }
     }
 }
-}
-//
